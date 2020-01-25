aab71c84595d6e3ac9da884e2f8ccb1bff854620
Gökhan Köse <gokhanksee@gmail.com>
2020-01-25 16:57:05 +0300

    Notlarımı ekledim

---
+++ Modellemeye Giriş.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Modellemeye giriş
+
+# Matematiksel model bir açıklama verir.Olayı ölçülebilir bir hale getirir.Prescriptive 'önerici'
+# Descriptive-İstatistik,EDA (explore data analysis)
+# Predictive-Machine Learning constrainlerini veriyor optimizasyon için
+# Prescriptive-Optimizasyon
+# 
+# 
+
+# ## Amaç Fonksiyonu,Kısıtlar,Karar Değişkenleri
+
+# Amaç Fonksiyonu:Bu modeli kurarken neyi hedefliyorum
+# Karar değişkenleri:Modelimde nerde olduğumu gösteren değişkenler    
+# Kısıtlar:Modelimin sınırlarını belirleyen değerler
+#     Bunlar sonucunda 
+#     stochastic:olasılığa bağlılık   
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
